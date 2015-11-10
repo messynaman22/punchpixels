940a4464699310dbe9dc62a4c316ba5e8c323898
Naman Modi <messynaman22@gmail.com>
2015-11-09 22:07:36 -0400

    adding missing migration

---
+++ database/migrations/2015_11_01_202030_alter_crm_admins_add_column_login_attemps.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterCrmAdminsAddColumnLoginAttemps extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('crm_admins', function(Blueprint $table)
+		{
+			$table->integer("login_attemps")->default(0);
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('crm_admins', function(Blueprint $table)
+		{
+			$table->dropColumn('login_attemps');
+		});
+	}
+
+}
